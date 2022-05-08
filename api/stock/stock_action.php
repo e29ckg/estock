47ccbdba5a2cae21b06f38badafa0934d0e3fc58
e29ckg <e29ckg@gmail.com>
2022-05-08 17:04:43 +0700

    Create stock_action.php

---
+++ api/stock/stock_action.php
@@ -0,0 +1,45 @@
+<?php
+include_once "../dbconfig.php";
+require "../auth/vendor/autoload.php";
+use \Firebase\JWT\JWT;
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+date_default_timezone_set("Asia/Bangkok");
+
+// $key = "__test_secret__";
+$jwt = null;
+// $databaseService = new DatabaseService();
+// $conn = $databaseService->getConnection();
+
+$data = json_decode(file_get_contents("php://input"));
+$stock = $data->stock[0];
+$password = "password";
+
+$authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+
+$arr = explode(" ", $authHeader);
+
+// http_response_code(200);
+// echo json_encode(array('status' => true, 'massege' => 'เพิ่มข้อมูลเรียบร้อย', 'responseJSON' => $stock_id ));
+// exit;           
+try{
+    $jwt = $arr[1];
+    $decoded = JWT::decode($jwt, base64_decode(strtr($key, '-_', '+/')), ['HS256']); 
+    $data = $decoded->data;
+
+    
+    
+        
+
+}catch(PDOException $e){
+    echo "Faild to connect to database" . $e->getMessage();
+    http_response_code(400);
+    echo json_encode(array('status' => 'error', 'massege' => 'เกิดข้อผิดพลาด..' . $e->getMessage()));
+}
+
+
