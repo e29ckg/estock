ad744544b91c2f57aeb446821522debe6bd1ecf4
e29ckg <e29ckg@gmail.com>
2022-05-12 16:07:54 +0700

    Create orders_by_user.php

---
+++ api/orders/orders_by_user.php
@@ -0,0 +1,99 @@
+<?php
+include_once "../dbconfig.php";
+require "../auth/vendor/autoload.php";
+use \Firebase\JWT\JWT;
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+date_default_timezone_set("Asia/Bangkok");
+
+// $key = "__test_secret__";
+$jwt = null;
+// $databaseService = new DatabaseService();
+// $conn = $databaseService->getConnection();
+
+$data = json_decode(file_get_contents("php://input"));
+$Ord_lists = $data->carts;
+
+$ord_own = '';
+
+$authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+
+$arr = explode(" ", $authHeader);
+
+http_response_code(200);
+echo json_encode(array('status' => 'success', 'massege' => 'เพิ่มข้อมูลเรียบร้อย', 'responseJSON' => $Ord->str_id ));
+die; 
+// empty($Ord->ord_date) ? $ord_date = date("Y-m-d h:s:i") : $ord_date = $Ord->ord_date;
+// try{
+//     $jwt = $arr[1];
+//     $decoded = JWT::decode($jwt, base64_decode(strtr($key, '-_', '+/')), ['HS256']); 
+//     $data_auth = $decoded->data;
+    
+//     $ord_own = $data_auth->fullname;    
+    
+//     if($Ord->action == 'insert'){   
+//         $dbcon->beginTransaction();
+//         $ord_id = time();
+        
+//         $sql = "INSERT INTO ords(ord_id, ord_date, ord_own) VALUE(:ord_id, :ord_date, :ord_own);";        
+//         $query = $dbcon->prepare($sql);
+//         $query->bindParam(':ord_id', $ord_id,PDO::PARAM_INT);
+//         $query->bindParam(':ord_date', $ord_date);
+//         $query->bindParam(':ord_own',$ord_own, PDO::PARAM_STR);
+//         $query->execute();  
+        
+//         $Ord_lists = $data->Ord_lists;
+//         foreach($Ord_lists as $ord_l){
+//             if($ord_l->pro_id != '' && $ord_l->qua != 0  && $ord_l->qua != ''){
+//                 $sql = "INSERT INTO ord_lists(ord_id, pro_id, pro_name, unit_name, qua, ord_own) VALUE(:ord_id, :pro_id, :pro_name, :unit_name, :qua, :ord_own);";        
+//                 $query = $dbcon->prepare($sql);
+//                 $query->bindParam(':ord_id', $ord_id, PDO::PARAM_INT);
+//                 $query->bindParam(':pro_id', $ord_l->pro_id, PDO::PARAM_INT);
+//                 $query->bindParam(':pro_name', $ord_l->pro_name, PDO::PARAM_STR);
+//                 $query->bindParam(':unit_name', $ord_l->unit_name, PDO::PARAM_STR);
+//                 $query->bindParam(':qua', $ord_l->qua, PDO::PARAM_INT);
+//                 $query->bindParam(':ord_own', $ord_own, PDO::PARAM_STR);
+//                 $query->execute();  
+//             }
+//         }
+//             // echo "เพิ่มข้อมูลเรียบร้อย ok";
+//         http_response_code(200);
+//         echo json_encode(array('status' => 'success', 'massege' => 'เพิ่มข้อมูลเรียบร้อย ok', 'responseJSON' => $Ord_lists));
+
+//         $dbcon->commit();
+//         exit;
+//     }
+
+    
+
+//     if($Ord->action == 'delete'){    
+//         $dbcon->beginTransaction();
+//         $sql = "DELETE FROM ords WHERE ord_id = $Ord->ord_id";
+//         $dbcon->exec($sql);
+
+//         $sql = "DELETE FROM Ord_lists WHERE ord_id = $Ord->ord_id";
+//         $dbcon->exec($sql);
+
+//         $dbcon->commit();
+//         http_response_code(200);
+//         echo json_encode(array('status' => 'success', 'massege' => 'Record deleted successfully'));  
+        
+//     }    
+
+// }catch(PDOException $e){
+//     if ($dbcon->inTransaction()) {
+//         $dbcon->rollback();
+//         // If we got here our two data updates are not in the database
+//     }
+
+//     echo "Faild to connect to database" . $e->getMessage();
+//     http_response_code(400);
+//     echo json_encode(array('status' => 'error', 'massege' => 'เกิดข้อผิดพลาด..' . $e->getMessage()));
+// }
+
+
