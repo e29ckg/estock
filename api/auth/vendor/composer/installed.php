ba8f1dfb715a81249e4a95dd3fa2e88bd64b6f35
e29ckg <e29ckg@gmail.com>
2025-10-28 20:37:57 +0700

    e

---
+++ api/auth/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jfroma/phptest',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6aedc612d9d781c8fe46d09e80d5c33fa6058e22',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bac0422822b92fe7a0ed1fc7b1b633d9efa37bae',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jfroma/phptest' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6aedc612d9d781c8fe46d09e80d5c33fa6058e22',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
